2e5d0cb74ddd62cc78962f0c1a08be9e27767b50
nadillustrator <nadin2208@gmail.com>
2022-09-28 00:52:37 +0300

    HW_SP2.7 - done with Map(addition)

--- src/main/java/pro/sky/courseworkrewright/service/EmployeeService.java
+++ src/main/java/pro/sky/courseworkrewright/service/EmployeeService.java
@@ -24,38 +24,37 @@ public class EmployeeService {
     }
 
     public Employee addEmployee(String name, String surname) {
-        Employee newEmployee = new Employee(name,surname);
+        Employee newEmployee = new Employee(name, surname);
         if (size <= employees.size()) {
             throw new EmployeeStorageIsFullException();
         }
-        if (employees.containsValue(newEmployee)){
+        if (employees.containsKey(newEmployee.getFullName())) {
             throw new EmployeeAlreadyAddException();
         }
         employees.put(newEmployee.getFullName(), newEmployee);
         return newEmployee;
     }
 
-    public Employee removeEmployee(String name, String surname){
+    public Employee removeEmployee(String name, String surname) {
         Employee newEmployee = new Employee(name, surname);
-        if(employees.containsValue(newEmployee)){
-            employees.remove(newEmployee.getFullName());
-            System.out.println("Employee "+ newEmployee.getName() + newEmployee.getSurname() +" is removed.");
-            return newEmployee;
+        if (employees.containsKey(newEmployee.getFullName())) {
+            System.out.println("Employee " + newEmployee.getFullName() + " is removed.");
+            return employees.remove(newEmployee.getFullName());
         }
         throw new EmployeeNotFoundException();
     }
 
-    public Employee findEmployee(String name, String surname){
+    public Employee findEmployee(String name, String surname) {
         Employee newEmployee = new Employee(name, surname);
-        if(employees.containsValue(newEmployee)){
-            System.out.println("Employee " + newEmployee.getName() + newEmployee.getSurname() + " found.");
+        if (employees.containsKey(newEmployee.getFullName())) {
+            System.out.println("Employee " + newEmployee.getFullName() + " found.");
             return newEmployee;
         }
 
         throw new EmployeeNotFoundException();
     }
 
-    public HashMap<String, Employee> showEmployees(){
+    public HashMap<String, Employee> showEmployees() {
         return new HashMap<>(employees);
     }
 
